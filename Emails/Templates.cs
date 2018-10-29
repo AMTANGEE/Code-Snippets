beb6da203e9f162f2a22874f97b47cdbc7cc94ee
rfraatz <rfraatz@amtangee.com>
2018-10-29 15:31:55 +0100

    added license and readme

---
+++ Emails/Templates.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Code_Snippets.Emails
+{
+    public class Templates
+    {
+        public AMTANGEE.SDK.Email.Template GetEmailTemplateByName(string name)
+        {
+            foreach(var t in new AMTANGEE.SDK.Email.Templates(AMTANGEE.SDK.Global.CurrentUser))
+            {
+                if (t.Name == name)
+                    return t;
+            }
+            return null;
+        }
+
+        public AMTANGEE.SDK.Email.Template GetEmailTemplateByGuid(Guid guid)
+        {
+            var t = new AMTANGEE.SDK.Email.Template(guid);
+            if (t.ExistsAndLoadedAndRights)
+                return t;
+
+            return null;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# AMTANGEE Code Snippets
+
+This is a collection of useful code snippets for developing with the AMTANGEE SDK.
+
+## Requirements
+
+* AMTANGEE SDK
+* .NET Framework 2.0
+
+## Feedback 
+
+* Send us an email at support@amtangee.com
+* File an issue here in [GitHub](https://github.com/AMTANGEE/Code-Snippets/issues)
+
+## License
+
+Copyright (c) AMTANGEE AG - https://www.amtangee.com
+
+Licensed under the [MIT](LICENSE) License.
